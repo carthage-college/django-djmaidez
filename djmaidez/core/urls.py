ed83786bc0fc4d69a883cba4919750cb6e54556a
Bender <plungerman@gmail.com>
2015-05-07 13:29:23 -0500

    updated urls paths

--- djmaidez/core/urls.py
+++ djmaidez/core/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^contact/', include('emergency.contact.urls')),
+    (r'^contact/', include('djmaidez.contact.urls')),
 )
