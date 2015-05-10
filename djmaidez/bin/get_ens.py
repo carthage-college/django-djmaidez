908f2442d3bc6c28861bb53fb62e738b13868c4f
Bender <plungerman@gmail.com>
2015-05-10 18:06:18 -0500

    updated shell script to fetch all instead of one, for testing

--- djmaidez/bin/get_ens.py
+++ djmaidez/bin/get_ens.py
@@ -13,16 +13,17 @@ for c in CODES:
     sql = "SELECT * FROM aa_rec WHERE aa = '%s' AND id='%s'" % (c,cid)
     result = do_esql(
         sql,key=settings.INFORMIX_DEBUG,
-        earl=settings.INFORMIX_EARL).fetchone()
+        earl=settings.INFORMIX_EARL)
     ens +=  "++%s++++++++++++++++++++++\n" % c
-    for f in FIELDS:
-        try:
-            if result[f]:
-                ens += "%s = %s\n" % (f, result[f])
-            else:
+    for r in result:
+        for f in FIELDS:
+            try:
+                if result[f]:
+                    ens += "%s = %s\n" % (f, result[f])
+                else:
+                    ens += "%s = \n" % (f)
+            except:
                 ens += "%s = \n" % (f)
-        except:
-            ens += "%s = \n" % (f)
-    print ens
-    ens = ""
+        print ens
+        ens = ""
 
