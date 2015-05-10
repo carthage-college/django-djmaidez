8bb04484949766e39dde6a37d7df0f88af707891
Bender <plungerman@gmail.com>
2015-05-10 09:27:10 -0500

    moved try/except within for loop

--- djmaidez/bin/get_ens.py
+++ djmaidez/bin/get_ens.py
@@ -1,8 +1,9 @@
-from djzbar.utils.informix import do_sql as esql
+from django.conf import settings
+from djzbar.utils.informix import do_sql as do_esql
 
-cid = ""
+cid = "1217657"
 
-fields = ['aa','beg_date','end_date','line1','line2','line3',
+FIELDS = ['aa','beg_date','end_date','line1','line2','line3',
 'phone','phone_ext','cell_carrier','opt_out']
 
 CODES = ['MIS1','MIS2','ICE','ICE2','ENS','MIS3']
@@ -10,12 +11,16 @@ CODES = ['MIS1','MIS2','ICE','ICE2','ENS','MIS3']
 ens = ""
 for c in CODES:
     sql = "SELECT * FROM aa_rec WHERE aa = '%s' AND id='%s'" % (c,cid)
-    try:
-        result = do_esql(
-            sql,key=settings.INFORMIX_DEBUG,
-            earl=settings.INFORMIX_EARL).fetchone()
-        ens +=  "++%s++++++++++++++++++++++\n" % c
-        for f in FIELDS:
+    result = do_esql(
+        sql,key=settings.INFORMIX_DEBUG,
+        earl=settings.INFORMIX_EARL).fetchone()
+    ens +=  "++%s++++++++++++++++++++++\n" % c
+    for f in FIELDS:
+        try:
             if result[f]:
                 ens += "%s = %s\n" % (f,result[f])
+        except:
+            ens += "%s = \n" % (f)
+    print ens
+    ens = ""
 
