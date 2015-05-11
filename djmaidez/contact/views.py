b141b73b79496fd4b189a09690f953acd069d4ce
Bender <plungerman@gmail.com>
2015-05-11 12:31:36 -0500

    reorganized the function list; save() now uses ORM

--- djmaidez/contact/views.py
+++ djmaidez/contact/views.py
@@ -10,6 +10,7 @@ from djtools.utils.database import row2dict
 from djmaidez.core.models import AARec, MOBILE_CARRIER, RELATIONSHIP
 
 import datetime
+
 EARL = settings.INFORMIX_EARL
 ENS_CODES = ['MIS1','MIS2','MIS3','ENS','ICE','ICE2']
 
@@ -20,279 +21,6 @@ def test(request):
     return render_to_response(
         "contact/test.html",{"uid":settings.DEFAULT_UID},
         context_instance=RequestContext(request)
-    )
-
-def json(request):
-    """
-    Called from javascript fill.js
-    """
-    template = "contact/modal.html"
-    t = loader.get_template(template)
-    data = {}
-    data["mobile_carrier"] = MOBILE_CARRIER
-    data["relationship"] = RELATIONSHIP
-    c = Context(data)
-    page = t.render(c)
-    json = {'emc':page}
-    page = simplejson.dumps(json)
-    return HttpResponse(
-        "successCallback(" + page + ")",
-        content_type="application/json; charset=utf-8"
-    )
-
-def save(request):
-
-    session = get_session(EARL)
-
-    MIS1_NAME = request.GET.get("MIS1_NAME", "")
-    MIS1_REL = request.GET.get("MIS1_REL", "")
-    MIS1_PHONE1 = request.GET.get("MIS1_PHONE1", "")
-    MIS1_PHONE2 = request.GET.get("MIS1_PHONE2", "")
-    MIS1_PHONE3 = request.GET.get("MIS1_PHONE3", "")
-    MIS2_NAME = request.GET.get("MIS2_NAME", "")
-    MIS2_PHONE1 = request.GET.get("MIS2_PHONE1", "")
-    MIS3_NAME = request.GET.get("MIS3_NAME", "")
-    MIS3_PHONE1 = request.GET.get("MIS3_PHONE1", "")
-    ENS_SELF_CELL = request.GET.get("ENS_SELF_CELL", "")
-    ENS_SMS = request.GET.get("ENS_SMS", "")
-    ENS_CARRIER = request.GET.get("ENS_CARRIER", "")
-    ENS_EMAIL = request.GET.get("ENS_EMAIL", "")
-    ICE_NAME = request.GET.get("ICE_NAME", "")
-    ICE_PHONE1 = request.GET.get("ICE_PHONE1", "")
-    ICE_PHONE2 = request.GET.get("ICE_PHONE2", "")
-    ICE_PHONE3 = request.GET.get("ICE_PHONE3", "")
-    ICE_REL = request.GET.get("ICE_REL", "")
-    ICE2_NAME = request.GET.get("ICE2_NAME", "")
-    USERID = request.GET.get("UserID", "")
-    ICE2_PHONE1 = request.GET.get("ICE2_PHONE1", "")
-    ICE2_PHONE2 = request.GET.get("ICE2_PHONE2", "")
-    ICE2_PHONE3 = request.GET.get("ICE2_PHONE3", "")
-    ICE2_REL = request.GET.get("ICE2_REL", "")
-
-    HOSTID = USERID
-    now = datetime.datetime.now()
-
-    end_date=''
-    this_year = now.year
-    next_year = this_year+1
-
-    if now.month > 1 and now.month < 9:
-        end_date = '11-01-'+str(this_year)
-    elif now.month > 8:
-        end_date = '04-01-'+str(next_year)
-    else:
-        end_date = '04-01-'+str(this_year)
-
-    if MIS1_NAME != '':
-        try:
-            mis1 = session.query(AARec).filter_by(id=HOSTID).\
-                filter_by(aa="MIS1").one()
-            sql = """
-                UPDATE
-                    aa_rec
-                SET
-                    line1='{}', cell_carrier='{}',
-                    phone='{}', line2='{}', line3='{}',
-                    beg_date='{}', end_date='{}'
-                WHERE
-                    aa = 'MIS1'
-                AND
-                    id = '{}'
-            """.format(
-                MIS1_NAME, MIS1_REL, MIS1_PHONE1, MIS1_PHONE2,
-                MIS1_PHONE3, now.strftime("%m-%d-%Y"), end_date, HOSTID
-            )
-            do_sql(sql)
-        except:
-            sql = """
-                INSERT INTO
-                    aa_rec (
-                        line1, cell_carrier, phone, line2, line3,
-                        id, aa, beg_date, end_date
-                    )
-                VALUES (
-                    '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'
-                )
-            """ % (
-                MIS1_NAME, MIS1_REL, MIS1_PHONE1, MIS1_PHONE2,
-                MIS1_PHONE3, HOSTID, 'MIS1', now.strftime("%m-%d-%Y"),
-                end_date
-            )
-            do_sql(sql)
-
-    if MIS2_NAME != '':
-        try:
-            mis2 = session.query(AARec).filter_by(id=HOSTID).\
-                filter_by(aa="MIS2").one()
-            sql = """
-                UPDATE
-                    aa_rec
-                SET
-                    line1='%s', phone='%s', beg_date='%s', end_date='%s'
-                WHERE
-                    aa = 'MIS2'
-                AND
-                    id = '%s'
-            """ % (
-                MIS2_NAME, MIS2_PHONE1, now.strftime("%m-%d-%Y"),
-                end_date, HOSTID
-            )
-            do_sql(sql)
-        except:
-            sql = """
-                INSERT INTO
-                    aa_rec (
-                        line1, phone, id, aa, beg_date, end_date
-                    )
-                VALUES (
-                    '%s', '%s', '%s', '%s', '%s', '%s'
-                )
-            """ % (
-                MIS2_NAME, MIS2_PHONE1, HOSTID, 'MIS2',
-                now.strftime("%m-%d-%Y"), end_date
-            )
-            do_sql(sql)
-
-    if MIS3_NAME != '':
-        try:
-            mis3 = session.query(AARec).filter_by(id=HOSTID).\
-                filter_by(aa="MIS3").one()
-            sql = """
-                UPDATE
-                    aa_rec
-                SET
-                    line1='%s', phone='%s', beg_date='%s', end_date='%s'
-                WHERE
-                    aa = 'MIS3'
-                AND
-                    id = '%s'
-            """ % (
-                MIS3_NAME, MIS3_PHONE1, now.strftime("%m-%d-%Y"),
-                end_date, HOSTID
-            )
-            do_sql(sql)
-        except:
-            sql = """
-                INSERT INTO
-                    aa_rec (
-                        line1, phone, id, aa, beg_date, end_date
-                    )
-                VALUES (
-                    '%s', '%s', '%s', '%s', '%s', '%s'
-                )
-            """ % (
-                MIS3_NAME, MIS3_PHONE1, HOSTID, 'MIS3',
-                now.strftime("%m-%d-%Y"), end_date
-            )
-            do_sql(sql)
-
-    if ENS_SMS != '':
-        try:
-            ens = session.query(AARec).filter_by(id=HOSTID).\
-                filter_by(aa="ENS").one()
-            sql = """
-                UPDATE
-                    aa_rec
-                SET
-                    opt_out='%s', phone='%s', cell_carrier='%s',
-                    line1='%s', beg_date='%s', end_date='%s'
-                WHERE
-                    aa = 'ENS'
-                AND
-                    id = '%s'
-            """ % (
-                ENS_SMS, ENS_SELF_CELL, ENS_CARRIER, ENS_EMAIL,
-                now.strftime("%m-%d-%Y"), end_date, HOSTID
-            )
-            do_sql(sql)
-        except:
-            sql = """
-                INSERT INTO
-                    aa_rec (
-                        opt_out, phone, cell_carrier, line1, id,
-                        aa, beg_date, end_date
-                    )
-                VALUES (
-                    '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'
-                )
-            """ % (
-                ENS_SMS, ENS_SELF_CELL, ENS_CARRIER, ENS_EMAIL,
-                HOSTID, 'ENS', now.strftime("%m-%d-%Y"), end_date
-            )
-            do_sql(sql)
-
-    if ICE_NAME != '':
-        try:
-            ice = session.query(AARec).filter_by(id=HOSTID).\
-                filter_by(aa="ICE").one()
-            sql = """
-                UPDATE
-                    aa_rec
-                SET
-                    line1='%s', phone='%s', line2='%s', line3='%s',
-                    cell_carrier='%s', beg_date='%s', end_date='%s'
-                WHERE
-                    aa = 'ICE'
-                AND
-                    id = '%s'
-            """ % (
-                ICE_NAME, ICE_PHONE1, ICE_PHONE2, ICE_PHONE3,
-                ICE_REL, now.strftime("%m-%d-%Y"), end_date, HOSTID
-            )
-            do_sql(sql)
-        except:
-            sql = """
-                INSERT INTO
-                    aa_rec (
-                        line1, phone, line2, line3, cell_carrier,
-                        id, aa, beg_date, end_date
-                    )
-                VALUES (
-                    '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'
-                )
-            """ % (
-                ICE_NAME, ICE_PHONE1, ICE_PHONE2, ICE_PHONE3,
-                ICE_REL, HOSTID, 'ICE', now.strftime("%m-%d-%Y"), end_date
-            )
-            do_sql(sql)
-
-    if ICE2_NAME != '':
-        try:
-            ice2 = session.query(AARec).filter_by(id=HOSTID).\
-                filter_by(aa="ENS").one()
-            sql = """
-                UPDATE
-                    aa_rec
-                SET
-                    line1='%s', phone='%s', line2='%s', line3='%s',
-                    cell_carrier='%s', beg_date='%s', end_date='%s'
-                WHERE
-                    aa = 'ICE2'
-                AND
-                    id = '%s'
-            """ % (
-                ICE2_NAME, ICE2_PHONE1, ICE2_PHONE2, ICE2_PHONE3,
-                ICE2_REL, now.strftime("%m-%d-%Y"), end_date, HOSTID
-            )
-            do_sql(sql)
-        except:
-            sql = """
-                INSERT INTO
-                    aa_rec (
-                        line1, phone, line2, line3, cell_carrier, id,
-                        aa, beg_date, end_date
-                    )
-                VALUES (
-                    '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')
-            """ % (
-                ICE2_NAME, ICE2_PHONE1, ICE2_PHONE2, ICE2_PHONE3, ICE2_REL,
-                HOSTID, 'ICE2', now.strftime("%m-%d-%Y"), end_date
-            )
-            do_sql(sql)
-
-    return HttpResponse(
-        'saveDone({"Status":"Success"})',
-        content_type="application/json; charset=utf-8"
     )
 
 def solo(request):
@@ -312,17 +40,37 @@ def solo(request):
     data["relationship"] = RELATIONSHIP
     data["solo"] = True
 
+    session.close()
+
     return render_to_response(
         "contact/solo.html", data,
         context_instance=RequestContext(request)
     )
 
+def json(request):
+    """
+    Called from javascript fill.js onload
+    """
+    template = "contact/modal.html"
+    t = loader.get_template(template)
+    data = {}
+    data["mobile_carrier"] = MOBILE_CARRIER
+    data["relationship"] = RELATIONSHIP
+    c = Context(data)
+    page = t.render(c)
+    json = {'emc':page}
+    page = simplejson.dumps(json)
+    return HttpResponse(
+        "successCallback(" + page + ")",
+        content_type="application/json; charset=utf-8"
+    )
+
 def populate(request):
-    HOSTID = request.GET.get("UserID", "")
+    cid = request.GET.get("UserID", "")
 
     session = get_session(EARL)
 
-    objs = session.query(AARec).filter_by(id=HOSTID).\
+    objs = session.query(AARec).filter_by(id=cid).\
         filter(AARec.aa.in_(ENS_CODES)).all()
     data = {}
     for e in ENS_CODES:
@@ -330,8 +78,102 @@ def populate(request):
     for o in objs:
         data[o.aa] = row2dict(o)
     retVal = simplejson.dumps(data)
+
+    session.close()
+
     return HttpResponse(
         'jsonResponcePopulate(' + retVal + ')',
         content_type="application/json; charset=utf-8"
     )
 
+def save(request):
+    """
+    Called from jquery ui modal construction. .getJSON in buttons.
+    """
+
+    # college ID
+    cid = request.GET.get("UserID", "")
+
+    # missing person 1
+    MIS1 = {
+        "aa": "MIS1",
+        "line1": request.GET.get("MIS1_NAME", ""),
+        "cell_carrier": request.GET.get("MIS1_REL", ""),
+        "phone": request.GET.get("MIS1_PHONE1", ""),
+        "line2": request.GET.get("MIS1_PHONE2", ""),
+        "line3": request.GET.get("MIS1_PHONE3", "")
+    }
+    # missing person 2
+    MIS2 = {
+        "aa": "MIS2",
+        "line1": request.GET.get("MIS2_NAME", ""),
+        "phone": request.GET.get("MIS2_PHONE1", "")
+    }
+    # missing person 3
+    MIS3 = {
+        "aa": "MIS3",
+        "line1": request.GET.get("MIS3_NAME", ""),
+        "phone": request.GET.get("MIS3_PHONE1", "")
+
+    }
+    # emergency notification system
+    ENS = {
+        "aa": "ENS",
+        "opt_out": request.GET.get("ENS_SMS", ""),
+        "phone": request.GET.get("ENS_SELF_CELL", ""),
+        "cell_carrier": request.GET.get("ENS_CARRIER", ""),
+        "line1": request.GET.get("ENS_EMAIL", "")
+    }
+    # in case of emergency 1
+    ICE = {
+        "aa": "ICE",
+        "line1": request.GET.get("ICE_NAME", ""),
+        "phone": request.GET.get("ICE_PHONE1", ""),
+        "line2": request.GET.get("ICE_PHONE2", ""),
+        "line3": request.GET.get("ICE_PHONE3", ""),
+        "cell_carrier": request.GET.get("ICE_REL", "")
+    }
+    # in case of emergency 2
+    ICE2 = {
+        "aa": "ICE2",
+        "line1": request.GET.get("ICE2_NAME", ""),
+        "phone": request.GET.get("ICE2_PHONE1", ""),
+        "line2": request.GET.get("ICE2_PHONE2", ""),
+        "line3": request.GET.get("ICE2_PHONE3", ""),
+        "cell_carrier": request.GET.get("ICE2_REL", "")
+    }
+
+    # begin and end dates
+    now = datetime.datetime.now()
+    this_year = now.year
+    next_year = this_year+1
+
+    if now.month > 1 and now.month < 9:
+        end_date = datetime.datetime(year=this_year, month=11, day=1)
+    elif now.month > 8:
+        end_date = datetime.datetime(year=next_year, month=04, day=1)
+    else:
+        end_date = datetime.datetime(year=this_year, month=04, day=1)
+
+    # instantiate our session
+    session = get_session(EARL)
+    # update else insert
+    for code in [MIS1, MIS2, MIS3, ENS, ICE, ICE2]:
+        code["beg_date"] = now
+        code["end_date"] = end_date
+        try:
+            obj = session.query(AARec).filter_by(id=cid).\
+                filter_by(aa = code["aa"]).one()
+            for key, value in code.iteritems():
+                setattr(obj, key, value)
+        except:
+            code["id"] = cid
+            a = AARec(**code)
+            session.add(a)
+    session.commit()
+    session.close()
+
+    return HttpResponse(
+        'saveDone({"Status":"Success"})',
+        content_type="application/json; charset=utf-8"
+    )
