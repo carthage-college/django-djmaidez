78dbc0cc9a86a607e9f9ed3ae05d32bbfed3cd06
Bender <plungerman@gmail.com>
2016-03-08 14:21:02 -0600

    wrapped structure in if/then logic to check for college id

--- djmaidez/contact/views.py
+++ djmaidez/contact/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import Context, RequestContext, loader
@@ -70,94 +70,98 @@ def save(request):
     cid = request.GET.get("UserID", "")
     # did the data come from medical forms project?
     djsani = request.GET.get("DJSANI","")
+    # no need to proceed if we don't have a college ID
+    if cid:
 
-    # missing person 1
-    MIS1 = {
-        "aa": "MIS1",
-        "line1": request.GET.get("MIS1_NAME", ""),
-        "cell_carrier": request.GET.get("MIS1_REL", ""),
-        "phone": request.GET.get("MIS1_PHONE1", ""),
-        "line2": request.GET.get("MIS1_PHONE2", ""),
-        "line3": request.GET.get("MIS1_PHONE3", "")
-    }
-    # missing person 2
-    MIS2 = {
-        "aa": "MIS2",
-        "line1": request.GET.get("MIS2_NAME", ""),
-        "phone": request.GET.get("MIS2_PHONE1", "")
-    }
-    # missing person 3
-    MIS3 = {
-        "aa": "MIS3",
-        "line1": request.GET.get("MIS3_NAME", ""),
-        "phone": request.GET.get("MIS3_PHONE1", "")
+        # missing person 1
+        MIS1 = {
+            "aa": "MIS1",
+            "line1": request.GET.get("MIS1_NAME", ""),
+            "cell_carrier": request.GET.get("MIS1_REL", ""),
+            "phone": request.GET.get("MIS1_PHONE1", ""),
+            "line2": request.GET.get("MIS1_PHONE2", ""),
+            "line3": request.GET.get("MIS1_PHONE3", "")
+        }
+        # missing person 2
+        MIS2 = {
+            "aa": "MIS2",
+            "line1": request.GET.get("MIS2_NAME", ""),
+            "phone": request.GET.get("MIS2_PHONE1", "")
+        }
+        # missing person 3
+        MIS3 = {
+            "aa": "MIS3",
+            "line1": request.GET.get("MIS3_NAME", ""),
+            "phone": request.GET.get("MIS3_PHONE1", "")
 
-    }
-    # emergency notification system
-    ENS = {
-        "aa": "ENS",
-        "opt_out": request.GET.get("ENS_SMS", ""),
-        "phone": request.GET.get("ENS_SELF_CELL", ""),
-        "cell_carrier": request.GET.get("ENS_CARRIER", ""),
-        "line1": request.GET.get("ENS_EMAIL", "")
-    }
-    # in case of emergency 1
-    ICE = {
-        "aa": "ICE",
-        "line1": request.GET.get("ICE_NAME", ""),
-        "phone": request.GET.get("ICE_PHONE1", ""),
-        "line2": request.GET.get("ICE_PHONE2", ""),
-        "line3": request.GET.get("ICE_PHONE3", ""),
-        "cell_carrier": request.GET.get("ICE_REL", "")
-    }
-    # in case of emergency 2
-    ICE2 = {
-        "aa": "ICE2",
-        "line1": request.GET.get("ICE2_NAME", ""),
-        "phone": request.GET.get("ICE2_PHONE1", ""),
-        "line2": request.GET.get("ICE2_PHONE2", ""),
-        "line3": request.GET.get("ICE2_PHONE3", ""),
-        "cell_carrier": request.GET.get("ICE2_REL", "")
-    }
+        }
+        # emergency notification system
+        ENS = {
+            "aa": "ENS",
+            "opt_out": request.GET.get("ENS_SMS", ""),
+            "phone": request.GET.get("ENS_SELF_CELL", ""),
+            "cell_carrier": request.GET.get("ENS_CARRIER", ""),
+            "line1": request.GET.get("ENS_EMAIL", "")
+        }
+        # in case of emergency 1
+        ICE = {
+            "aa": "ICE",
+            "line1": request.GET.get("ICE_NAME", ""),
+            "phone": request.GET.get("ICE_PHONE1", ""),
+            "line2": request.GET.get("ICE_PHONE2", ""),
+            "line3": request.GET.get("ICE_PHONE3", ""),
+            "cell_carrier": request.GET.get("ICE_REL", "")
+        }
+        # in case of emergency 2
+        ICE2 = {
+            "aa": "ICE2",
+            "line1": request.GET.get("ICE2_NAME", ""),
+            "phone": request.GET.get("ICE2_PHONE1", ""),
+            "line2": request.GET.get("ICE2_PHONE2", ""),
+            "line3": request.GET.get("ICE2_PHONE3", ""),
+            "cell_carrier": request.GET.get("ICE2_REL", "")
+        }
 
-    # begin and end dates
-    now = datetime.datetime.now()
-    this_year = now.year
-    next_year = this_year+1
+        # begin and end dates
+        now = datetime.datetime.now()
+        this_year = now.year
+        next_year = this_year+1
 
-    if now.month > 1 and now.month < 9:
-        end_date = datetime.datetime(year=this_year, month=11, day=1)
-    elif now.month > 8:
-        end_date = datetime.datetime(year=next_year, month=04, day=1)
+        if now.month > 1 and now.month < 9:
+            end_date = datetime.datetime(year=this_year, month=11, day=1)
+        elif now.month > 8:
+            end_date = datetime.datetime(year=next_year, month=04, day=1)
+        else:
+            end_date = datetime.datetime(year=this_year, month=04, day=1)
+
+        # instantiate our session
+        session = get_session(EARL)
+        # update else insert
+        for code in [MIS1, MIS2, MIS3, ENS, ICE, ICE2]:
+            code["beg_date"] = now
+            code["end_date"] = end_date
+            try:
+                obj = session.query(AARec).filter_by(id=cid).\
+                    filter_by(aa = code["aa"]).one()
+                for key, value in code.iteritems():
+                    setattr(obj, key, value)
+            except:
+                code["id"] = cid
+                a = AARec(**code)
+                session.add(a)
+
+        # medical forms data?
+        if djsani:
+            from djsani.core.utils import get_manager
+            manager = get_manager(session, cid)
+            manager.emergency_contact=True
+
+        session.commit()
+        session.close()
+
+        return HttpResponse(
+            'saveDone({"Status":"Success"})',
+            content_type="application/json; charset=utf-8"
+        )
     else:
-        end_date = datetime.datetime(year=this_year, month=04, day=1)
-
-    # instantiate our session
-    session = get_session(EARL)
-    # update else insert
-    for code in [MIS1, MIS2, MIS3, ENS, ICE, ICE2]:
-        code["beg_date"] = now
-        code["end_date"] = end_date
-        try:
-            obj = session.query(AARec).filter_by(id=cid).\
-                filter_by(aa = code["aa"]).one()
-            for key, value in code.iteritems():
-                setattr(obj, key, value)
-        except:
-            code["id"] = cid
-            a = AARec(**code)
-            session.add(a)
-
-    # medical forms data?
-    if djsani:
-        from djsani.core.utils import get_manager
-        manager = get_manager(session, cid)
-        manager.emergency_contact=True
-
-    session.commit()
-    session.close()
-
-    return HttpResponse(
-        'saveDone({"Status":"Success"})',
-        content_type="application/json; charset=utf-8"
-    )
+        raise Http404
