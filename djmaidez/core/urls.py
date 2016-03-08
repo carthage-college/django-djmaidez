ad4bf4c12ecdfc3dc6b1789ce62207d71e47c75c
Bender <plungerman@gmail.com>
2016-03-08 14:20:12 -0600

    added 404 and 500 server errors

--- djmaidez/core/urls.py
+++ djmaidez/core/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include
 
+handler404 = 'djtools.views.errors.four_oh_four_error'
+handler500 = 'djtools.views.errors.server_error'
+
 urlpatterns = patterns('',
     (r'^contact/', include('djmaidez.contact.urls')),
 )
