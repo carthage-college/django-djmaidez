7cf34eed41a7917e4adbf751a31d6572f81e0820
plungerman <plungerman@gmail.com>
2018-06-18 15:14:43 -0500

    added auth URLs

--- djmaidez/core/urls.py
+++ djmaidez/core/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth import views as auth_views
 
 from djauth.views import loggedout
