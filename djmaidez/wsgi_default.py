0ddda5b3d722d50c28a95933a4789b9b051ea3a6
plungerman <plungerman@gmail.com>
2019-03-13 14:41:19 -0500

    updates for migration to virtual environment on new ubuntu 18.04 server, hygiea

--- djmaidez/wsgi_default.py
+++ djmaidez/wsgi_default.py
@@ -1,16 +1,18 @@
-import os, sys
+import os
+import time
+import traceback
+import signal
+import sys
 
-# Python
-sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/usr/lib/python2.7/dist-packages/')
-sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-sys.path.append('/data2/django_1.8/')
-sys.path.append('/usr/local/django_projects/')
-sys.path.append('/usr/local/django_third/')
-# Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'djmaidez.settings'
-os.environ.setdefault("PYTHON_EGG_CACHE", "/var/cache/python/.python-eggs")
-os.environ.setdefault("TZ", "America/Chicago")
+# python
+sys.path.append('/data2/python_venv/2.7/djmaidez/lib/python2.7/')
+sys.path.append('/data2/python_venv/2.7/djmaidez/lib/python2.7/site-packages/')
+sys.path.append('/data2/python_venv/2.7/djmaidez/lib/django_projects/')
+sys.path.append('/data2/python_venv/2.7/djmaidez/lib/django-djmaidez/')
+# django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djmaidez.settings')
+os.environ.setdefault('PYTHON_EGG_CACHE', '')
+os.environ.setdefault('TZ', 'America/Chicago')
 # informix
 os.environ['INFORMIXSERVER'] = ''
 os.environ['DBSERVERNAME'] = ''
@@ -18,8 +20,19 @@ os.environ['INFORMIXDIR'] = ''
 os.environ['ODBCINI'] = ''
 os.environ['ONCONFIG'] = ''
 os.environ['INFORMIXSQLHOSTS'] = ''
-os.environ['LD_LIBRARY_PATH'] = ''
-os.environ['LD_RUN_PATH'] = ''
+os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules:$INFORMIXDIR/lib/cli'
+os.environ['LD_RUN_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules'
 # wsgi
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+# NOTE: remove the try/except in production
+#application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
+    exit(-1)
