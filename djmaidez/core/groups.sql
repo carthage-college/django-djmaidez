dee65e6e0c380124e61e9b328a5ca293ab3939ca
plungerman <plungerman@gmail.com>
2020-02-26 13:23:17 -0600

    first commit after beginning migration from pytho 2.7 to 3.6

---
+++ djmaidez/core/groups.sql
@@ -0,0 +1,8 @@
+INSERT INTO
+    `auth_group` (`id`, `name`)
+VALUES
+    (1, 'carthageStudentStatus'),
+    (2, 'carthageFacultyStatus'),
+    (3, 'carthageStaffStatus'),
+    (4, 'carthageFormerStudentStatus'),
+    (5, 'carthageOtherStatus');
---
+++ djmaidez/settings/base.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+
+"""Django settings for project."""
+
+import os
+
+from djimix.settings.local import DBSERVERNAME
+from djimix.settings.local import INFORMIX_ODBC
+from djimix.settings.local import INFORMIX_ODBC_TRAIN
+from djimix.settings.local import INFORMIXDIR
+from djimix.settings.local import INFORMIXSERVER
+from djimix.settings.local import INFORMIXSQLHOSTS
+from djimix.settings.local import LD_LIBRARY_PATH
+from djimix.settings.local import LD_RUN_PATH
+from djimix.settings.local import MSSQL_EARL
+from djimix.settings.local import ODBCINI
+from djimix.settings.local import ONCONFIG
+
+
+DEBUG = True
+INFORMIX_DEBUG = False
+REQUIRED_ATTRIBUTE = True
+ADMINS = (
+    ('', ''),
+)
+MANAGERS = ADMINS
+SECRET_KEY = None
+# databases
+DATABASES = {
+    'default': {
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'django_djmaidez',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': '',
+        'PASSWORD': '',
+    },
+}
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+TIME_ZONE = 'America/Chicago'
+TIME_INPUT_FORMATS = ('%H:%M %p',)
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_TZ = True
+USE_I18N = True
+USE_L10N = True
+DEFAULT_CHARSET = 'utf-8'
+FILE_CHARSET = 'utf-8'
+ROOT_URLCONF = 'djmaidez.core.urls'
+SERVER_URL = 'www.carthage.edu'
+API_URL = '{0}/{1}'.format(SERVER_URL, 'api')
+LIVEWHALE_API_URL = 'https://{0}'.format(SERVER_URL)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(__file__)
+ROOT_URL = '/emergency/'
+MEDIA_ROOT = '{0}/assets/'.format(BASE_DIR)
+MEDIA_URL = '/media/djmaidez/'
+format(ROOT_URL)
+STATIC_ROOT = '{0}/static/'.format(ROOT_DIR)
+STATIC_URL = 'https://{0}/static/djmaidez/'.format(SERVER_URL)
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like '/home/html/static' or 'C:/www/django/static'.
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.cache.FetchFromCacheMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    # Add the automatic auth middleware just after the default
+    # AuthenticationMiddleware that manages sessions and cookies
+    # 'djauth.middleware.AutomaticUserLoginMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # the following should be uncommented unless you are
+    # embedding your apps in iframes
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+# template stuff
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), 'templates'),
+            '/data2/django_templates/djkorra/',
+            '/data2/django_templates/djcher/',
+            '/data2/django_templates/',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'debug': DEBUG,
+            'context_processors': [
+                'djtools.context_processors.sitevars',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'djmaidez',
+)
+# LDAP Constants
+LDAP_SERVER = ''
+LDAP_PORT = ''
+LDAP_PROTOCOL = ''
+LDAP_BASE = ''
+LDAP_USER = ''
+LDAP_PASS = None
+LDAP_OBJECT_CLASS = ''
+LDAP_GROUPS = None
+LDAP_RETURN = ()
+LDAP_ID_ATTR = ''
+LDAP_AUTH_USER_PK = False
+LDAP_EMAIL_DOMAIN = ''
+LDAP_OBJECT_CLASS_LIST = []
+LDAP_GROUPS = {}
+LDAP_RETURN = []
+LDAP_ID_ATTR = ''
+LDAP_AUTH_USER_PK = False
+# SMTP settings
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_FAIL_SILENTLY = True
+DEFAULT_FROM_EMAIL = ''
+SERVER_EMAIL = ''
+SERVER_MAIL = ''
+# auth backends
+AUTHENTICATION_BACKENDS = (
+    'djauth.ldapBackend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+LOGIN_URL = '{0}accounts/login/'.format(ROOT_URL)
+LOGOUT_URL = '{0}accounts/logout/'.format(ROOT_URL)
+LOGIN_REDIRECT_URL = '{0}dashboard/'.format(ROOT_URL)
+USE_X_FORWARDED_HOST = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_DOMAIN = ''
+SESSION_COOKIE_NAME = 'django_djsani_cookie'
+SESSION_COOKIE_AGE = 86400
+LOG_FILEPATH = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'logs/',
+)
+LOG_FILENAME = '{0}{1}'.format(LOG_FILEPATH, 'debug.log')
+DEBUG_LOG_FILENAME = '{0}{1}'.format(LOG_FILEPATH, 'debug.log')
+INFO_LOG_FILENAME = '{0}{1}'.format(LOG_FILEPATH, 'info.log')
+ERROR_LOG_FILENAME = '{0}{1}'.format(LOG_FILEPATH, 'error.log')
+CUSTOM_LOG_FILENAME = '{0}{1}'.format(LOG_FILEPATH, 'custom.log')
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s',
+            'datefmt': '%Y/%b/%d %H:%M:%S',
+        },
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s',
+            'datefmt': '%Y/%b/%d %H:%M:%S',
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s',
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': LOG_FILENAME,
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'include_html': True,
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'custom_logfile': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'],  # don't run logger in production
+            'class': 'logging.FileHandler',
+            'filename': CUSTOM_LOG_FILENAME,
+            'formatter': 'custom',
+        },
+        'info_logfile': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'backupCount': 10,
+            'maxBytes': 50000,
+            'filters': ['require_debug_false'],  # run logger in production
+            'filename': INFO_LOG_FILENAME,
+            'formatter': 'simple',
+        },
+        'debug_logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': DEBUG_LOG_FILENAME,
+            'formatter': 'verbose',
+        },
+        'error_logfile': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'],  # don't run logger in production
+            'class': 'logging.FileHandler',
+            'filename': ERROR_LOG_FILENAME,
+            'formatter': 'verbose',
+        },
+        'djauth': {
+            'handlers': ['logfile'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
