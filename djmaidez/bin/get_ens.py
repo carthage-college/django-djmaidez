ef82d627057251a1b7514f43e4ddbfe7beba61ee
plungerman <plungerman@gmail.com>
2020-07-16 09:59:56 -0500

    updates for python 3.6

--- djmaidez/bin/get_ens.py
+++ djmaidez/bin/get_ens.py
@@ -1,29 +1,39 @@
+# -*- coding: utf-8 -*-
+
 from django.conf import settings
-from djzbar.utils.informix import do_sql as do_esql
+from djimix.core.utils import get_connection
+from djimix.core.utils import xsql
 
-cid = ""
 
-FIELDS = ['aa','beg_date','end_date','line1','line2','line3',
-'phone','phone_ext','cell_carrier','opt_out']
+cid = ''
+fields = (
+    'aa',
+    'beg_date',
+    'end_date',
+    'line1',
+    'line2',
+    'line3',
+    'phone',
+    'phone_ext',
+    'cell_carrier',
+    'opt_out',
+)
+codes = ('MIS1', 'MIS2', 'ICE', 'ICE2', 'ENS', 'MIS3')
 
-CODES = ['MIS1','MIS2','ICE','ICE2','ENS','MIS3']
-
-ens = ""
-for c in CODES:
-    sql = "SELECT * FROM aa_rec WHERE aa = '%s' AND id='%s'" % (c,cid)
-    result = do_esql(
-        sql,key=settings.INFORMIX_DEBUG,
-        earl=settings.INFORMIX_EARL)
-    ens +=  "++%s++++++++++++++++++++++\n" % c
-    for r in result:
-        for f in FIELDS:
+ens = ''
+for code in codes:
+    sql = 'SELECT * FROM aa_rec WHERE aa = "{0}" AND id="{1}"'.format(code, cid)
+    with get_connection(settings.INFORMIX_ODBC) as connection:
+        rows = xsql(sql, connection).fetchall()
+    ens += '++{0}++++++++++++++++++++++\n'.format(code)
+    for row in rows:
+        for field in fields:
             try:
-                if result[f]:
-                    ens += "%s = %s\n" % (f, result[f])
+                if row[field]:
+                    ens += '{0} = {1}\n'.format(field, row[field])
                 else:
-                    ens += "%s = \n" % (f)
-            except:
-                ens += "%s = \n" % (f)
-        print ens
+                    ens += '{0} = \n'.format(field)
+            except Exception:
+                ens += '{0} = \n'.format(field)
+        print(ens)
         ens = ""
-
